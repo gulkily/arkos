1f3485e67892876619040af03c28f7e4ae2e9cea
duck_master <duckeagle1280@gmail.com>
2025-06-18 01:18:25 -0400

    start implementing /v1/chat/completions mock

--- frontend/vitest-setup-client.ts
+++ frontend/vitest-setup-client.ts
@@ -21,13 +21,18 @@ Object.defineProperty(window, 'matchMedia', {
 const fetchMocker: FetchMock = createFetchMock(vi);
 
 // a silly route for sanity-checking
-// TODO: replace `localhost:3000` by actual domain name once we get one
+// NOTE: replace `localhost:3000` by actual domain name once we get one
 fetchMocker.mockResponse((req: Request) => {
 	// TODO: move this code into its own file
-	if (req.url == "https://localhost:3000/vfm-mock") {
+	if (req.url === 'https://localhost:3000/vfm-mock') {
 		return new Response('lorem ipsum dolor sit amet', { status: 200 });
-	}
-	else {
+	} else if (req.url === 'https://localhost:3000/v1/chat/completions') {
+		if (req.method !== 'POST') {
+			return new Response('wrong HTTP method!', { status: 405 });
+		}
+		// TODO: input validation
+		return new Response('TODO', { status: 501 }); // NOTE: HTTP 501 = "not implemented"
+	} else {
 		return new Response('not found', { status: 404 });
 	}
 });
