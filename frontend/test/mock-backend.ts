9678fc64a0ba0cf477ee1e411fd1ac479d9b7583
duck_master <duckeagle1280@gmail.com>
2025-06-18 01:30:38 -0400

    create handleChatCompletions helper function

--- frontend/test/mock-backend.ts
+++ frontend/test/mock-backend.ts
@@ -1,4 +1,19 @@
 // NOTE: replace `localhost:3000` by actual domain name once we get one
+import assert from 'node:assert';
+
+/**
+ * Handles *only* requests to POST /v1/chat/completions
+ * @param req the HTTP request to handle
+ * @returns the HTTP response to send
+ */
+function handleChatCompletions(req: Request): Response {
+	assert.strictEqual(req.url, 'https://localhost:3000/v1/chat/completions');
+	if (req.method !== 'POST') {
+		return new Response('wrong HTTP method!', { status: 405 });
+	}
+	// TODO: input validation and construct an appropriate response
+	return new Response('TODO', { status: 501 }); // NOTE: HTTP 501 = "not implemented"
+}
 
 /**
  * Handles all HTTP requests to the mock backend
@@ -9,11 +24,7 @@ function handleMockBackendRequest(req: Request): Response {
 	if (req.url === 'https://localhost:3000/vfm-mock') {
 		return new Response('lorem ipsum dolor sit amet', { status: 200 });
 	} else if (req.url === 'https://localhost:3000/v1/chat/completions') {
-		if (req.method !== 'POST') {
-			return new Response('wrong HTTP method!', { status: 405 });
-		}
-		// TODO: input validation
-		return new Response('TODO', { status: 501 }); // NOTE: HTTP 501 = "not implemented"
+		return handleChatCompletions(req);
 	} else {
 		return new Response('not found', { status: 404 });
 	}
