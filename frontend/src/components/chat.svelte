9be9b63c0d1a284831c29ec4c7cc0a094d98267f
duck_master <duckeagle1280@gmail.com>
2025-06-25 23:08:03 -0400

    add dummy event handler

--- frontend/src/components/chat.svelte
+++ frontend/src/components/chat.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	/* TODO: add a script here */
+	// c.f. https://lightningchart.com/js-charts/api-documentation/v5.2.0/types/MouseEventHandler.html for typing
+	function handleMessageSending(event: MouseEvent): void {
+		/* TODO: replace this with a real event listener */
+		console.log(`hello from ${event.toString()}, it's ${Date.now()}`);
+	}
 </script>
 
 <div class="chat-container" data-testid="chat-container">
@@ -12,6 +16,6 @@
 	<div class="chat-input-container" data-testid="chat-input-container">
 		<input type="text" class="chat-input" id="chatInput" placeholder="Type a message..." />
 		<!-- TODO: attach an event listener to this button to call the backend -->
-		<button class="send-button" id="sendButton">Send</button>
+		<button class="send-button" id="sendButton" onclick={handleMessageSending}>Send</button>
 	</div>
 </div>
