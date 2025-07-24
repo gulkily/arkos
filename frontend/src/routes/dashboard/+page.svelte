44048cd9c98d76548684d6cb8e6c8527b6f3a7d3
duck_master <duckeagle1280@gmail.com>
2025-07-23 20:25:34 -0400

    move main UI to /dashboard URL

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,9 +1,9 @@
 <!-- NOTE: do NOT transclude the stylesheet here! This WILL break the test cases! -->
 
 <script lang="ts">
-	import Calendar from '../components/Calendar.svelte';
-	import Chat from '../components/chat.svelte';
-	import Navbar from '../components/navbar.svelte';
+	import Calendar from '../../components/Calendar.svelte';
+	import Chat from '../../components/chat.svelte';
+	import Navbar from '../../components/navbar.svelte';
 </script>
 
 <Navbar />
