a5822118cd755a2bfaf4acb8885dd2fe0edcf9fb
duck_master <duckeagle1280@gmail.com>
2025-07-23 21:03:59 -0400

    update navbar

--- frontend/src/components/navbar.svelte
+++ frontend/src/components/navbar.svelte
@@ -2,16 +2,15 @@
 <!-- This is static for now, so it doesn't need to be tested -->
 <nav class="navbar">
 	<!-- NOTE: this CSS class is necessary, otherwise it conflicts with the navbar in the calendar view -->
-	<!-- svelte-ignore a11y_invalid_attribute-->
 	<a href="/">Home</a>
 	<!-- TODO: replace these links -->
 	<a href="https://example.com">About</a>
 	<div class="dropdown">
 		<button class="dropbtn">Features </button>
 		<div class="dropdown-content">
-			<a href="https://example.com">Link 1</a>
-			<a href="https://example.com">Link 2</a>
-			<a href="https://example.com">Link 3</a>
+			<a href="/dashboard">Dashboard</a>
+			<a href="https://example.com">AI chat</a>
+			<a href="https://example.com">Calendar</a>
 		</div>
 	</div>
 	<a href="https://github.com/SGIARK/ARK2.0">Source code</a>
