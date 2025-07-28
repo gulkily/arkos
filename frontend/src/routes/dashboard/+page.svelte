99ecff9fb460560a86d483df5ab22852efd5d3ea
duck_master <duckeagle1280@gmail.com>
2025-07-28 18:49:11 -0400

    make dashboard route into a real dashboard?

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,13 +1,36 @@
-<!-- NOTE: do NOT transclude the stylesheet here! This WILL break the test cases! -->
-
+<!-- TODO: test after replacing static content -->
 <script lang="ts">
-	import Calendar from '../../components/Calendar.svelte';
-	import Chat from '../../components/chat.svelte';
 	import Navbar from '../../components/navbar.svelte';
 </script>
 
 <Navbar />
 <div class="app-container" data-testid="app-container">
-	<Calendar />
-	<Chat />
+	<section>
+		<h1>ARK 2.0</h1>
+	</section>
+
+	<section>
+		<h2>Happy night, [username]!</h2>
+		<!-- TODO: replace with dynamic content -->
+		<time>[date]</time>
+		<address>[city]</address>
+	</section>
+
+	<section>
+		<h2 id="events">🗓️ Events</h2>
+		<ul>
+			<li>[event 1]</li>
+			<li>[event 2]</li>
+		</ul>
+		<p><small>(View all!)</small></p>
+	</section>
+
+	<section>
+		<h2 id="chats">💬 Chats</h2>
+		<ul>
+			<li>[Chat 1]</li>
+			<li>[Chat 2]</li>
+		</ul>
+		<p><small>(View all!)</small></p>
+	</section>
 </div>
