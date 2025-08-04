6488fe2477ef33b6a0cab992e03abdd2067c09ac
duck_master <duckeagle1280@gmail.com>
2025-08-04 17:29:44 -0400

    fix ul/li confusion in about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -8,22 +8,22 @@
 	<section>
 		<h2>Features</h2>
 		<p>ARK is MIT's central productivity hub. It currently has the following 2 features:</p>
-		<li>
-			<ul>A personal calendar</ul>
-			<ul>
+		<ul>
+			<li>A personal calendar</li>
+			<li>
 				A chat interface for interacting with the ARK assistant - an intelligent, LLM-based, and <i
 					>personalized</i
 				> agent that knows just what you want.
-			</ul>
-		</li>
+			</li>
+		</ul>
 		<p>We are planning on adding more features in the future.</p>
 	</section>
 	<section>
 		<h2>Team</h2>
-		<li>
-			<ul>Nathaniel Morgan</ul>
-			<ul>Joshua Guo</ul>
-			<ul>Ilya Gulko</ul>
-		</li>
+		<ul>
+			<li>Nathaniel Morgan</li>
+			<li>Joshua Guo</li>
+			<li>Ilya Gulko</li>
+		</ul>
 	</section>
 </main>
